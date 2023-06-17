ffd85733b3cbce448ee907d46635fb3222ac1d25
Milszut <cmochmilosz@gmail.com>
2023-06-17 15:53:56 +0200

    Home navigation menu fix

--- app/src/main/java/com/example/newsflash/Home.kt
+++ app/src/main/java/com/example/newsflash/Home.kt
@@ -1,20 +1,90 @@
 package com.example.newsflash
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.example.newsflash.databinding.FragmentHomeBinding
+import org.json.JSONException
 
-class Home : Fragment() {
+class Home : Fragment(), OnNewsClick {
 
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var mAdaptor: NewsAdaptor
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val recyclerView: RecyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        mAdaptor = NewsAdaptor(this)
+        recyclerView.adapter = mAdaptor
+
+        fetchnews()
+    }
+
+    private fun fetchnews() {
+        val queue = Volley.newRequestQueue(requireContext())
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                try {
+                    val newsJsonArray = response.getJSONArray("articles")
+                    val newsArray = ArrayList<News>()
+                    for (i in 0 until newsJsonArray.length()) {
+                        val newsJsonObject = newsJsonArray.getJSONObject(i)
+                        val news = News(
+                            newsJsonObject.getString("title"),
+                            newsJsonObject.getString("author"),
+                            newsJsonObject.getString("url"),
+                            newsJsonObject.getString("urlToImage")
+                        )
+                        newsArray.add(news)
+                    }
+                    mAdaptor.updateData(newsArray)
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            },
+            Response.ErrorListener {
+                // Obsługa błędu żądania
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
+
+    companion object {
+        fun newInstance(): Home {
+            return Home()
+        }
+    }
+
+    override fun onClicked(news: News) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(requireContext(), Uri.parse(news.url))
+    }
 }
+
+
--- app/src/main/java/com/example/newsflash/MainActivity.kt
+++ app/src/main/java/com/example/newsflash/MainActivity.kt
@@ -1,99 +1,34 @@
 package com.example.newsflash
 
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.example.newsflash.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), OnNewsClick {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var mAdaptor: NewsAdaptor
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Home())
 
-        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        replaceFragment(Home.newInstance())
 
-        mAdaptor = NewsAdaptor(this)
-        recyclerView.adapter = mAdaptor
-
-        fetchnews()
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-
-            when(it.itemId){
-
-                R.id.home -> replaceFragment(Home())
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> replaceFragment(Home.newInstance())
                 R.id.profile -> replaceFragment(Profile())
                 R.id.settings -> replaceFragment(Contact())
-
-                else ->{
-
-
-
-                }
-
             }
-
             true
-
         }
-
-
     }
 
-    fun fetchnews(){
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET,url,null,
-            Response.Listener {
-                val newsJsonArray = it.getJSONArray("articles")
-                val newsArray = ArrayList<News>()
-                for(i in 0 until newsJsonArray.length()) {
-                    val newsJsonObject = newsJsonArray.getJSONObject(i)
-                    val news = News(
-                        newsJsonObject.getString("title"),
-                        newsJsonObject.getString("author"),
-                        newsJsonObject.getString("url"),
-                        newsJsonObject.getString("urlToImage")
-                    )
-                    newsArray.add(news)
-                }
-                mAdaptor.updateData(newsArray)
-            },
-            Response.ErrorListener {
-            }
-        )
-        queue.add(jsonObjectRequest)
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
-
-    override fun onClicked(news: News) {
-        val builder = CustomTabsIntent.Builder()
-        val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(this,Uri.parse(news.url))
-    }
-
-    private fun replaceFragment(fragment : Fragment){
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
-        fragmentTransaction.commit()
-
-
-    }
-
 }
