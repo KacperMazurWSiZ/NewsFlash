86e6dd4fc1b3cf5a96ef8a21bb640a7c5a9651c2
Kacper <w66057@student.wsiz.edu.pl>
2023-06-17 11:21:51 +0200

    bottom menu

--- app/src/main/java/com/example/newsflash/MainActivity.kt
+++ app/src/main/java/com/example/newsflash/MainActivity.kt
@@ -4,19 +4,24 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.example.newsflash.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnNewsClick {
 
     private lateinit var mAdaptor: NewsAdaptor
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
         val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -25,6 +30,28 @@ class MainActivity : AppCompatActivity(), OnNewsClick {
         recyclerView.adapter = mAdaptor
 
         fetchnews()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.settings -> replaceFragment(Contact())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
     }
 
     fun fetchnews(){
@@ -58,4 +85,15 @@ class MainActivity : AppCompatActivity(), OnNewsClick {
         val customTabsIntent = builder.build()
         customTabsIntent.launchUrl(this,Uri.parse(news.url))
     }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+
 }
